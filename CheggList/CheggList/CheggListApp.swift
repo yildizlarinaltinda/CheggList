7eef504097f9a76d3c7fe5857852a65740eb0582
hasan-yildizz <yildizhn2@gmail.com>
2022-06-27 10:10:23 -0400

    Setup Project Done

---
+++ CheggList/CheggList/CheggListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheggListApp.swift
+//  CheggList
+//
+//  Created by Hasan Yildiz on 6/26/22.
+//
+
+import SwiftUI
+
+@main
+struct CheggListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheggList/CheggList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CheggList
+//
+//  Created by Hasan Yildiz on 6/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
